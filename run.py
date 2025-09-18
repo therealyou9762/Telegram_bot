03a57479c259f8e9d430d8589fbdfd9941e0242a
therealyou9762 <113407303+therealyou9762@users.noreply.github.com>
2025-09-18 22:30:48 +0300

    Run web and bot in separate threads

--- run.py
+++ run.py
@@ -1,9 +1,13 @@
-from database import init_db
+import threading
+from web.app import app as flask_app
+from bot.bot import main as bot_main
 
+def start_web():
+    flask_app.run(host="0.0.0.0", port=5000)
 
-def main():
-    init_db()
-
+def start_bot():
+    bot_main()
 
 if __name__ == "__main__":
-    main()
+    threading.Thread(target=start_web).start()
+    threading.Thread(target=start_bot).start()
