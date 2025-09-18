462504b87c72344d2e4bdcf7623c9c0802402e78
therealyou9762 <113407303+therealyou9762@users.noreply.github.com>
2025-09-18 23:26:18 +0300

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,19 @@
-import threading
+from flask import Flask
+from db.models import db
+from flask_migrate import Migrate
+
 from web.app import app as flask_app
 from bot.bot import main as bot_main
+import threading
+import os
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+flask_app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
+
+db.init_app(flask_app)
+migrate = Migrate(flask_app, db)
 
 def start_web():
     flask_app.run(host="0.0.0.0", port=5000)
